afcde4847d2fb9be2e6e16c66b66d84740595dbf
yuande <miraclecome@gmail.com>
2016-03-22 23:41:08 +0800

    fix bug

--- rational.py
+++ rational.py
@@ -10,7 +10,7 @@ class Rational(object):
         >>> a = Rational(3, 4)
         >>> b = Rational(1, 3)
         >>> c = a + b
-        >>> print c
+        >>> print(c)
     """
 
     def __init__(self, numerator, denominator):
@@ -24,14 +24,18 @@ class Rational(object):
             doing gcd in __add__ rather than __str__,
             because language like c don't have high-precision number like python
         """
-        _denominator = self.denomiator * obj.denomiator
-        _numerator = self.numerator * obj.denominator + obj.numerator * self.denomiator
+        _numerator = self.numerator * obj.denominator + obj.numerator * self.denominator
+        _denominator = self.denominator * obj.denominator
         ret = self.gcd(_numerator, _denominator)
         self.numerator = _numerator // ret
         self.denominator = _denominator // ret
         return self
 
     def __str__(self):
+        ret = self.gcd(self.numerator, self.denominator)
+        
+        if self.denominator // ret == 1:
+            return '{}'.format(self.numerator // ret)
         return '{}/{}'.format(self.numerator, self.denominator)
 
     def gcd(self, m, n):
@@ -43,4 +47,10 @@ class Rational(object):
         for i in range(border, 0, -1):
             if m % i == n % i == 0:
                 return i
+
+if __name__ == '__main__':
+    a = Rational(3, 4)
+    b = Rational(1, 3)
+    c = a + b
+    print(c)
 
